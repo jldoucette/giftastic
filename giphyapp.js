cb5a5e944bc2dedd210d09c8982232736974dd7d
jldoucette <jd@jonathandoucette.com>
2017-04-28 20:07:58 -0700

    Final Files

--- giphyapp.js
+++ giphyapp.js
@@ -6,122 +6,87 @@
     var noAnimateURL;
     var rating;
 
-  function setUp(){
-  console.log("Starting setUp");
-    $("#buttons").empty();
-    for (var j=0;j<topics.length;j++){
-    console.log("inside for loop of setup, j is "+ j);
-    var buttonInit=$("<button>");
-    buttonInit.addClass("topics");
-    buttonInit.attr("data-topic", topics[j]);
-    buttonInit.text(topics[j]);
-    $("#buttons").append(buttonInit);
-  }
+    function setUp(){
+          $("#buttons").empty();
+          for (var j=0;j<topics.length;j++){
+          var buttonInit=$("<button>");
+          buttonInit.addClass("topics btn-lg");
+          buttonInit.attr("data-topic", topics[j]);
+          buttonInit.text(topics[j]);
+          $("#buttons").append(buttonInit);
+        }
           clicked();
-          // clickImage();
-          // addHero();
-}
+    }
 
-   console.log("Document ready");
-   function clicked() {
-    console.log("Function clicked started");
-    $("button").on("click", function() {
-      console.log("Clicked Button");
-      event.preventDefault();
-      $("#gifs").empty();
-      var queryLimit=10;
-      var ratingFilter="g";
-      var topicSearch = $(this).attr("data-topic");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topicSearch + "&api_key=dc6zaTOxFJmzC&limit="+queryLimit+"&rating="+ratingFilter;
-      
-        console.log("starting AJAX query");
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        .done(function(response) {
-          var results = response.data;
+       function clicked() {
+           $("button").on("click", function() {
+            event.preventDefault();
+            $("#gifs").empty();
 
-          console.log(results);
-          for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
-            animateURL=results[i].images.fixed_width.url;
-            noAnimateURL=results[i].images.fixed_width_still.url;
-            rating=results[i].rating;  
-            console.log(animateURL);
-            console.log(noAnimateURL);
-            console.log(rating);
+            var queryLimit=10;
+            var ratingFilter="g";
+            var topicSearch = $(this).attr("data-topic");
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topicSearch + "&api_key=dc6zaTOxFJmzC&limit="+queryLimit+"&rating="+ratingFilter;
             
-            var ratingText = $("<p>").text("Rating: " + rating);
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+              })
+                .done(function(response) {
+                  var results = response.data;
 
-            var gifImage = $("<img>");
-            var imageID="image"+i;
-            gifImage.attr("data-animate", animateURL);
-            gifImage.attr("data-noanimate", noAnimateURL);
-            gifImage.attr("src", noAnimateURL);
-            gifImage.attr("data-state", "still");
-            // gifImage.attr("data-state","animate");
-            gifImage.addClass("gif");
-            gifImage.attr("id",imageID);
-            gifDiv.prepend(ratingText);
-            gifDiv.prepend(gifImage);
+                      for (var i = 0; i < results.length; i++) {
+                        var gifDiv = $("<div class='item'>");
+                        animateURL=results[i].images.fixed_width.url;
+                        noAnimateURL=results[i].images.fixed_width_still.url;   
+                        rating=results[i].rating;  
+                        
+                        var ratingText = $("<p>").text("Rating: " + rating);
 
-            $("#gifs").prepend(gifDiv);
-            console.log("attempted to prepend");
-            }
-        });
-      });
-  }
+                        var gifImage = $("<img>");
+                        var imageID="image"+i;
+                        gifImage.attr("data-animate", animateURL);
+                        gifImage.attr("data-noanimate", noAnimateURL);
+                        gifImage.attr("src", noAnimateURL);
+                        gifImage.attr("data-state", "still");
+                        gifImage.addClass("gif");
+                        gifImage.attr("id",imageID);
+                        gifDiv.prepend(ratingText);
+                        gifDiv.prepend(gifImage);
 
-// function clickImage() {
-  console.log("Click Image Started");
+                        $("#gifs").prepend(gifDiv);
+                        }
+                });
+            });
+      }
+
           $("#gifs").on("click", "img", function() {
-            console.log('clicked ' + $(this));
             var itemClicked=$(this).attr("id");
             var itemClickedStatus=$(this).attr("data-state");
             var itemClickedAnimate=$(this).attr("data-animate");
             var itemClickedNoAnimate=$(this).attr("data-noanimate");
 
-
-      console.log("clicked on image " + itemClicked);
-      console.log("Status is " + itemClickedStatus);
       event.preventDefault();
     
-      if (itemClickedStatus=="still") {
-      $(this).attr("src",itemClickedAnimate);
-      $(this).attr("data-state","animate");
-      console.log("Changed to Animate " + itemClickedAnimate);
-    }
-      else {
-        console.log(this);
-       $(this).attr("src",itemClickedNoAnimate);
-       $(this).attr("data-state","still");
+            if (itemClickedStatus=="still") {
+            $(this).attr("src",itemClickedAnimate);
+            $(this).attr("data-state","animate");
+          }
+            else {
+             $(this).attr("src",itemClickedNoAnimate);
+             $(this).attr("data-state","still");
+            }
+          });
 
-        console.log("Changed to No Animate " + itemClickedNoAnimate);
-      }
-    });
-
-
-  // }
-
- // function addHero() {
-  console.log("Add Hero Started");
-   $("#add-hero").on("click", function(event) {
-        event.preventDefault();
-        var hero = $("#hero-input").val().trim();
-        topics.push(hero);
-        setUp();
-        console.log("setUp started from addHero");
-        console.log("About to start clicked from addHero");
-        clicked();
-
+       $("#add-hero").on("click", function(event) {
+            event.preventDefault();
+            var hero = $("#hero-input").val().trim();
+            topics.push(hero);
+            setUp();
 
       });
 
-// }
-  console.log("about to start setUp at bottom");
   setUp();
-
   
     });
    
